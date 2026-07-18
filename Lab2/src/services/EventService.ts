b14d6c4b04322f7a339ebd938abb4e8143bd3614
Katikalockers <saipan1150@gmail.com>
2026-07-18 21:32:34 +0700

    Lab3: 1.4

--- Lab2/src/services/EventService.ts
+++ Lab2/src/services/EventService.ts
@@ -12,5 +12,8 @@ const apiClient = axios.create({
 export default {
     getEvents() {
         return apiClient.get('/events')
+    },
+    getEventListeners(id: number) {
+    return apiClient.get('/events/' + id)
     }
 }
