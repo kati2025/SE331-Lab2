b55bb63c0c259f00302f889ffad1592160756b7a
Katikalockers <saipan1150@gmail.com>
2026-07-20 02:22:01 +0700

    4.2*

--- Lab2/src/views/event/RegisterView.vue
+++ Lab2/src/views/event/RegisterView.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
 import { toRefs } from 'vue'
 import { type Event } from '@/types'
+import { useRouter } from 'vue-router';
 
 const props = defineProps<{
     event: Event
 }>()
 // eslint-disable-next-line
 const { event } = toRefs(props)
+const router = useRouter()
+const register = () => {
+    //If
+    // Push
+    router.push({ name: 'event-detail-view' })
+}
 </script>
 <template>
-    <p>Regidter event here</p>
+    <p>Register event here</p>
+    <button @click="register">Register</button>
 </template>
