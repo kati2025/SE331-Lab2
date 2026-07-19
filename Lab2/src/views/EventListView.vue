2727b24e8f61904d7e88940c330f07d49d4f075d
Katikalockers <saipan1150@gmail.com>
2026-07-19 18:54:28 +0700

    before 2.7

--- Lab2/src/views/EventListView.vue
+++ Lab2/src/views/EventListView.vue
@@ -6,11 +6,7 @@ import { ref, onMounted, computed, watchEffect } from 'vue'
 import EventService from '@/services/EventService'
 
 const events = ref<Event[] | null>(null)
-const totalEvents = ref<number>(0)
-  const hasNextPage = computed(() => {
-  const totalPages = Math.ceil(totalEvents.value / 2)
-    return page.value < totalPages
-  })
+
 const props = defineProps({
   page: {
     type: Number,
@@ -24,7 +20,6 @@ onMounted (() => {
     EventService.getEvents(2, page.value)
       .then((response) => {
         events.value = response.data
-        totalEvents.value = response.headers['x-total-count']
       })
       .catch((error) => {
         console.error('There was an error!', error)
@@ -49,7 +44,6 @@ onMounted (() => {
   <RouterLink 
     :to="{ name: 'event-list-view', query: { page: page + 1 } }"
     rel="next"
-    v-if="hasNextPage"
     >Next Page</RouterLink
     >
 </template>
