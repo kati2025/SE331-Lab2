adaa37dd6b9c433cc2691175218c07788cbe97d9
Katikalockers <saipan1150@gmail.com>
2026-08-09 21:39:10 +0700

    4.1

--- Lab2/src/components/EventCard.vue
+++ Lab2/src/components/EventCard.vue
@@ -19,13 +19,14 @@ defineProps<{
 
 <template>
   <RouterLink class="event-link" :to="{ name: 'event-detail-view', params: {id: event.id}}">
-    <div class="event-card">
+    <div class="p-5 w-[250px] cursor-pointer border border-[#39495c] mb-[18px]
+hover:scale-101 hover:shadow-sp">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
   </RouterLink>
 </template>
-
+/* p=ระยะห่างขอบ, mb=Margin Bottom ระยะล่างนอกกรอบ */
 <style  scoped>
 .event-class {
   display: flex;
